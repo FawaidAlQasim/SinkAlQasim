2f12ced792ecc754dfa71afbf7250394a81ad278
فوائد القاسم <FawaidAlQasim@gmail.com>
2025-09-21 19:49:46 +0200

    Create bulk-upload.vue
    add bulk upload page

---
+++ app/pages/dashboard/bulk-upload.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bulk-upload-page">
+    <BulkUpload />
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'dashboard',
+  middleware: 'auth'
+})
+</script>
